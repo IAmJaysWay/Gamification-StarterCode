cca2420e155500c44d0138383fe134818cb50c98
IAmJaysWay <juhani.ahokas@gmail.com>
2022-01-20 22:26:20 +0200

    First commit

---
+++ src/components/Gamify.jsx
@@ -0,0 +1,9 @@
+export default function Gamify() {
+    
+  return (
+      <div>
+        hi
+      </div>
+    );
+}
+
---
+++ src/hooks/useCollectors.js
@@ -0,0 +1,6 @@
+
+const useUsers = () => {
+ 
+};
+
+export default useUsers;
